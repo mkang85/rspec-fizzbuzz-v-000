2526fee133c1adf854077c5f3ed513e76195bc79
Michael Kang <michaelkang85@gmail.com>
2018-01-26 02:42:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   if num % 3 && num % 5
-    puts 'FizzBuzz'
+    puts "FizzBuzz"
   elsif num % 5
-    puts 'Buzz'
+    puts "Buzz"
   elsif num % 3
-    puts 'Fizz'
+    puts "Fizz"
   else !(num % 3) || !(num % 5)
-    puts 'nil'
+    puts "nil"
 end
