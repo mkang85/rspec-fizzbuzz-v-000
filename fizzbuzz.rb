7e8936e5f58a4658dc3512135ddc72ca956eb99b
Michael Kang <michaelkang85@gmail.com>
2018-01-26 02:49:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,5 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-     "Fizz"
-   elsif num % 5 == 0
-     "Buzz"
-   else num % 5 == 0 && num % 3 == 0
-     "FizzBuzz"
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
   end
   end
